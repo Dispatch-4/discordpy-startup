91a9bc5fb14d77d2fd709d2611b633a3ec880c04
Dispatch-4 <74116505+Dispatch-4@users.noreply.github.com>
2020-11-08 06:26:21 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -15,22 +15,6 @@ async def on_command_error(ctx, error):
 from discord.ext import tasks
 import discord
 
-client = discord.Client()
-
-channel_sent = 746047616046858322
-
-@tasks.loop(hour=1)
-async def send_message_every_1h():
-    await channel_sent.send("!cnow")
-
-@client.event
-async def on_ready():
-    global channel_sent 
-    channel_sent = client.get_channel(746047616046858322)
-    send_message_every_1h.start()
-    
-client.run()
-    
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
