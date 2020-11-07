749424feb6357bcf88b2be2b447a60ce901cfea3
Dispatch-4 <74116505+Dispatch-4@users.noreply.github.com>
2020-11-08 06:29:07 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -19,3 +19,32 @@ async def ping(ctx):
     await ctx.send('pong')
 
 bot.run(token)
+
+import discord
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print("on_ready")
+    print(discord.__version__)
+
+client.run(Nzc0NzA3MjI0NzIzNDU2MDIx.X6bsUw.4V8BcjfC2LXD2fvG07Fzk6nn2f4)
+
+from discord.ext import tasks
+import discord
+
+client = discord.Client()
+
+channel_sent = None
+
+@tasks.loop(seconds=10)
+async def send_message_every_10sec():
+    await channel_sent.send("10秒経ったよ")
+
+    @client.event
+async def on_ready():
+    global channel_sent 
+    channel_sent = client.get_channel(any_channel_id)
+    send_message_every_10sec.start()
+
+client.run("hogehogetoken")
