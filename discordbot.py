37257da6389fede926614b953e256d87f4439397
Dispatch-4 <74116505+Dispatch-4@users.noreply.github.com>
2020-11-08 06:08:24 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -12,14 +12,6 @@ async def on_command_error(ctx, error):
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
 
-@client.event
-async def on_ready():
-    while True:
-        if time.strftime('%H:%M:%S',time.localtime())=='06:05:00':
-            channel = client.get_channel('748898203918663780')
-            await client.send_message(channel, '06:05:00')
-            sleep(5)
-
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
