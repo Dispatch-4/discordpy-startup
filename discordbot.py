9d5e9c1358ddaf85025f8d3d515ef84c00126d8c
Dispatch-4 <74116505+Dispatch-4@users.noreply.github.com>
2020-11-08 12:04:24 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,11 +1,9 @@
 from discord.ext import tasks
 import os
 import traceback
-import discord
 
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
-
 client = discord.Client()
 
 channel_sent = 746047616046858322
@@ -14,8 +12,8 @@ channel_sent = 746047616046858322
 async def send_message_every_60min():
     await channel_sent.send("!cnow")
 
-@client.event
-async def on_ready():
+@bot.event
+async def command(start):
     global channel_sent 
     channel_sent = client.get_channel(746047616046858322)
     send_message_every_60min.start()
